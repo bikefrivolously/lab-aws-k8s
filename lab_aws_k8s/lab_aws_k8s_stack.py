ec68f4e29900b34238f9a83cd00f966f3838ef0f
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-02-11 10:16:47 -0500

    set up fck-nat and vpc

--- lab_aws_k8s/lab_aws_k8s_stack.py
+++ lab_aws_k8s/lab_aws_k8s_stack.py
@@ -1,19 +1,19 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
 )
 from constructs import Construct
+import aws_cdk.aws_ec2 as ec2
+import cdk_fck_nat
+
 
 class LabAwsK8SStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LabAwsK8SQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        nat_provider = cdk_fck_nat.FckNatInstanceProvider(
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.T4G, ec2.InstanceSize.NANO
+            )
+        )
+        vpc = ec2.Vpc(self, "k8-vpc", nat_gateway_provider=nat_provider)
